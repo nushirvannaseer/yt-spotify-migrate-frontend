57a4e60d4f1eb58d70301e29b407325a68560f59
Nushirvan Naseer <nushirvannaseer@gmail.com>
2024-09-21 22:51:56 +0500

    made the app to completely run in dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,8 +34,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="dark"
           disableTransitionOnChange
         >
           <QueryProvider>
